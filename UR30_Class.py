5ad310755646209c4f46de1a035ea71194dc72e8
Diego Bravo Pérez <a2270193@correo.uia.mx>
2026-05-11 16:15:32 -0600

    Commit que crea el comentario para empezar a resolver un problema de control Cinemático

--- UR30_Class.py
+++ UR30_Class.py
@@ -1,4 +1,4 @@
-from numpy import cos, sin , array, eye, pi, deg2rad
+from numpy import cos, sin , array, eye, pi, deg2rad, acos, matrix, rad2deg, dot
 
 class UR30:
 
@@ -20,23 +20,27 @@ class UR30:
         
         self.T = self.CinematicaDirecta()
 
-        self.Pos = self.Posicion()  
+        self.Pos = self.Posicion()
+
+        self.Ori = self.Orientacion()  
     
     """
-    Función que obtiene la cinemática directa del UR30
+    Función que obtiene la cinemática directa del UR30 mediante el método de DH y transformación de matrices
 
     """
 
     def CinematicaDirecta(self):
          T = eye(4)
 
+         #Aplica la matriz de transformación obtenida con el método de DH
          for i in range(6):
             Aj = array([[cos(self.θ[i]), -sin(self.θ[i])*cos(self.__α[i]), sin(self.θ[i])*sin(self.__α[i]), self.__a[i]*cos(self.θ[i])],
                         [sin(self.θ[i]), cos(self.θ[i])*cos(self.__α[i]), -cos(self.θ[i])*sin(self.__α[i]), self.__a[i]*sin(self.θ[i])],
                         [0,              sin(self.__α[i]),                 cos(self.__α[i]),                self.__d[i]],
                         [0,              0,                                0,                               1]])
             T @= Aj
-
+         
+         #Se aplica la transformación de matrices
          T @= self.__tool 
 
          return T 
@@ -49,3 +53,33 @@ class UR30:
     def Posicion(self):
         return self.T[:3, 3] * 1000
     
+
+    """
+    Función que obtiene la orientación en eje-ángulo (estandar de Universal Robotics) del robot.
+    
+    """
+    
+    def Orientacion(self):
+        #Se obtiene la matriz de rotación del efector final con respecto a la base
+        mat_rot = self.T[:3, :3]
+
+        θ = acos((matrix.trace(mat_rot) - 1) / 2 )
+
+        #u = (1 / (2 sin theta)) * [ R32 - R23; R13 - R31; R21 - R12 ]
+        A = 1/(2*sin(θ))
+        B = [mat_rot[2, 1] - mat_rot[1, 2],
+             mat_rot[0, 2] - mat_rot[2, 0],
+             mat_rot[1, 0] - mat_rot[0, 1]]
+        
+        u = dot(A, B)
+
+        r_ur = u*θ
+        r_ur_deg = rad2deg(r_ur)
+
+        return r_ur_deg
+
+    """
+    Función que lleva al robot UR30 a la posición deseada para comenzar con el proceso de pintado.
+    Esta función utiliza control cinemático.
+    """
+        
--- main.py
+++ main.py
@@ -6,3 +6,6 @@ RDK = Robolink()
 limpiador = RDK.Item('UR30')
 
 ur30 = UR30(limpiador.Joints().tolist())
+
+print(ur30.Pos)
+print(ur30.Ori)
