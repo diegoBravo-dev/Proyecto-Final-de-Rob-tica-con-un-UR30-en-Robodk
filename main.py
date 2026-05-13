dd828b1742a750552f6337575b019c2717c1df11
Diego Bravo Pérez <a2270193@correo.uia.mx>
2026-05-12 23:07:32 -0600

    Se implementó el polinomio quintico para trazar trayectorias con el resultado del ángulo obtenido por LM

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from robodk.robolink import Robolink
 from UR30_Class import UR30
+from trayectorias import trazarTrayectoria
 from numpy import deg2rad, array
 from time import sleep
 
 RDK = Robolink()
 
-limpiador = RDK.Item('UR30')
+pintor = RDK.Item('UR30')
+spray = RDK.Item('Generic Paint Sprayer')
 
-ur30 = UR30(limpiador.Joints().tolist())
+pintor.setTool(spray)
 
+ur30 = UR30(pintor.Joints().tolist())
 
 print("--- OBTENIENDO POSICIÓN Y ORIENTACIÓN ACTUAL ----\n")
 sleep(2)
@@ -20,9 +23,9 @@ sleep(2)
 
 print("\n--- MOVIENDO ROBOT A LA POSICIÓN INICIAL DE LA TAREA ----\n")
 
-nueva_pos = ur30.posicionarRobot(deg2rad(array(limpiador.Joints())))
+nueva_pos = ur30.posicionarRobot(deg2rad(array(pintor.Joints())))
 
-limpiador.MoveJ(nueva_pos)
+pintor.MoveJ(nueva_pos)
 
 sleep(2)
 
@@ -31,3 +34,26 @@ print("\n¡Movimiento completado!\n")
 sleep(2)
 
 siguiente_pos = ur30.CinematicaInversa(-0.310192, -1.402855, 0.498155, [-22.583, -134.362, 85.773])
+
+print(ur30.θ)
+
+t, q, w, a = trazarTrayectoria(ur30.θ, siguiente_pos)
+
+q1 = q[0]
+q2 = q[1]
+q3 = q[2]
+q4 = q[3]
+q5 = q[4]
+q6 = q[5]
+
+spray.setParam("Activate", 1)
+
+for i in range(len(t)):
+    θ1 = q1[i]
+    θ2 = q2[i]
+    θ3 = q3[i]
+    θ4 = q4[i]
+    θ5 = q5[i]
+    θ6 = q6[i]
+    movimiento = [θ1, θ2, θ3, θ4, θ5, θ6]
+    pintor.MoveJ(movimiento)
